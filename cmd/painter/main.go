6ff9a370229ccc2b15074bc6456d56ebd44070f9
MaksFed <maksimfedorenko71@gmail.com>
2025-06-10 23:53:42 +0300

    created entrypoint for app

--- cmd/painter/main.go
+++ cmd/painter/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/manabr0w/golang-GUI/painter"
+	"github.com/manabr0w/golang-GUI/painter/lang"
+	"github.com/manabr0w/golang-GUI/ui"
+)
+
+func main() {
+	var (
+		pv ui.Visualizer
+
+		opLoop painter.Loop
+		parser lang.Parser
+	)
+
+	pv.Title = "Simple painter"
+
+	pv.OnScreenReady = opLoop.Start
+	opLoop.Receiver = &pv
+
+	go func() {
+		http.Handle("/", lang.HttpHandler(&opLoop, &parser))
+		_ = http.ListenAndServe("localhost:17000", nil)
+	}()
+
+	pv.Main()
+	opLoop.StopAndWait()
+}
